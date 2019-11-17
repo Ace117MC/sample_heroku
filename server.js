f6efbffa0767e389ddb828d7dde0f99dfd589375
Anmol Bansal <bansal.anmol1999@gmail.com>
2019-11-17 17:52:53 +0530

    heroku error
    Signed-off-by: Anmol Bansal <bansal.anmol1999@gmail.com>

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-const app = require('express')()
+const express = require('express')
+
+const app = express()
 
 const PORT = process.env.PORT || 5555
-
-app.use('/' , express.static(__dirname + '/public'))
 
 app.get('/' , (req,res) => {
     res.send('<h1>Heroku Start !!!</h1>')
 })
-
-app.post('/search' , async (req,res) => {
-    res.redirect(`www.google.com/search?q=`+req.body.search)
-})
-
-
-
 
 app.listen(PORT)
